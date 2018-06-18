02476e31b10cbc6da804ee4e28adb54a3a5835d3
Jonathan Berg <jberg@netik.de>
2018-06-18 16:13:53 +0200

    Add IpAddress validation

---
+++ TuyaLocal/Validations/IpAddressAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace TuyaLocal.Validations
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Net;
+
+    public class IpAddressAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(
+            object value,
+            ValidationContext validationContext)
+        {
+            var ip = (IPAddress) value;
+
+            return Equals(ip, IPAddress.Loopback)
+                ? new ValidationResult(ErrorMessage)
+                : ValidationResult.Success;
+        }
+    }
+}
