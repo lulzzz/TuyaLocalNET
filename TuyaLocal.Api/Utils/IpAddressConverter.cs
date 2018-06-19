be7fe2a327797447a1a5f9d8e41259a8c1e12919
Jonathan Berg <jberg@NETIK.DE>
2018-06-19 12:59:43 +0200

    Add ipaddres to json converter

---
+++ TuyaLocal.Api/Utils/IpAddressConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace TuyaLocal.Api.Utils
+{
+    using System;
+    using System.Net;
+    using Newtonsoft.Json;
+
+    public class IpAddressConverter
+        : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) =>
+            objectType == typeof(IPAddress);
+
+        public override void WriteJson(
+            JsonWriter writer,
+            object value,
+            JsonSerializer serializer) => writer.WriteValue(value.ToString());
+
+        public override object ReadJson(
+            JsonReader reader,
+            Type objectType,
+            object existingValue,
+            JsonSerializer serializer) =>
+            IPAddress.Parse((string) reader.Value);
+    }
+}
