ff83d54b26146855046c869e94a3e6e3e233c706
Jonathan Berg <jberg@NETIK.DE>
2018-06-19 12:52:01 +0200

    Add remove command annoations
    Add required and string length attributes to RemoveDevice command.

--- TuyaLocal/Commands/RemoveDevice.cs
+++ TuyaLocal/Commands/RemoveDevice.cs
@@ -1,10 +1,14 @@
 ﻿namespace TuyaLocal.Commands
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RemoveDevice
     {
         public RemoveDevice(string id) =>
             Id = id;
 
+        [Required]
+        [StringLength(16, MinimumLength = 10)]
         public string Id { get; }
     }
 }
